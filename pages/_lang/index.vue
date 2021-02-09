4640f6b9e337b61bf3f1cc0a0c0e627ef2bc2ff3
Pablo Giralt <pablogiralt@gmail.com>
2021-02-09 22:00:11 +0100

    Base pages site structure

---
+++ pages/_lang/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <slice-zone
+      type="page"
+      uid="homepage"
+      :lang="lang"
+    />
+  </div>
+</template>
+
+<script>
+import SliceZone from 'vue-slicezone'
+
+export default {
+  components: {
+    SliceZone
+  },
+  computed: {
+    lang () {
+      const currentLang = this.$route.name === 'index' ? 'es' : this.$route.name
+      const locales = {
+        en: 'en-us',
+        es: 'es-es'
+      }
+      return locales[currentLang]
+    }
+  }
+}
+</script>
