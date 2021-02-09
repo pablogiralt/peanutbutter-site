9fad1e59ebb8047f2d8312744e6b18ec01270dfe
Pablo Giralt <pablogiralt@gmail.com>
2021-02-09 21:43:48 +0100

    setup prismic slicemachine

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,38 +5,48 @@ export default {
     htmlAttrs: {
       lang: 'en'
     },
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
-    ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    meta: [{
+      charset: 'utf-8'
+    }, {
+      name: 'viewport',
+      content: 'width=device-width, initial-scale=1'
+    }, {
+      hid: 'description',
+      name: 'description',
+      content: ''
+    }],
+    link: [{
+      rel: 'icon',
+      type: 'image/x-icon',
+      href: '/favicon.ico'
+    }],
+    script: [{
+      "src": "https://cdn.polyfill.io/v2/polyfill.min.js?features=Element.prototype.classList"
+    }, {
+      "src": "https://cdn.jsdelivr.net/npm/focus-visible@5.0.2/dist/focus-visible.min.js"
+    }]
   },
-
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-  ],
-
+  css: ["vue-essential-slices/src/styles/styles.scss"],
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-  ],
-
+  plugins: [],
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
-
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: [
-    // https://go.nuxtjs.dev/eslint
-    '@nuxtjs/eslint-module'
-  ],
-
+  buildModules: [// https://go.nuxtjs.dev/eslint
+  '@nuxtjs/eslint-module'],
   // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [
-  ],
-
+  modules: [["@nuxtjs/prismic", {
+    "endpoint": "https://peanutbutter-site.cdn.prismic.io/api/v2",
+    "apiOptions": {
+      "routes": [{
+        "type": "page",
+        "path": "/:uid"
+      }]
+    }
+  }], ["nuxt-sm"]],
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
+    transpile: ["vue-slicezone", "nuxt-sm"]
   }
-}
+};
