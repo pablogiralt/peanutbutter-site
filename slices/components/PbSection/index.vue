9ea19bddd1e36f487263554bf3d6aca3b229fa2f
Pablo Giralt <pablogiralt@gmail.com>
2021-03-08 21:20:01 +0100

    #1 Create vue component for banners

---
+++ slices/components/PbSection/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <component
+    v-bind:is="'section'"
+    :class="`${classMain} ${darkMode ? classMain + '--black' : ''} ${classAttr ? classAttr : ''}`"
+    v-bind="theme"
+  >
+    <slot/>
+  </component>
+</template>
+<script>
+import { commonProps } from '../../utils'
+
+export default {
+  name: 'PbSection',
+  props: {
+    theme: commonProps.theme,
+    darkMode: commonProps.darkMode,
+    classAttr: {
+      type: String,
+      required: false,
+      default: null
+    },
+    classMain: {
+      type: String,
+      required: false,
+      default: null
+    }
+  }
+}
+</script>
