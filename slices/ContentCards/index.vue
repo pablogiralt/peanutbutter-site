057d53cdb57f83524ab8f76ab38848bf3ff62439
Pablo Giralt <pablogiralt@gmail.com>
2021-03-09 15:10:11 +0100

    #9 Create Content cards Vue Component

---
+++ slices/ContentCards/index.vue
@@ -0,0 +1,50 @@
+<template>
+  <pb-section
+    v-bind="theme.wrapper"
+    class-main="content-cards"
+    :class-attr="''"
+  >
+    <div class="content-cards__item" v-for="(item, i) in slice.items" :key="`slice-item-${i}`">
+      <prismic-image class="content-cards__image" :field="item.image" />
+      <prismic-rich-text :field="item.title" />
+      <prismic-rich-text :field="item.subtitle" />
+      <prismic-rich-text :field="item.description" />
+    </div>
+  </pb-section>
+</template>
+
+<script>
+import { PbSection } from '../components'
+import { commonProps } from '../utils'
+
+export default {
+  components: {
+    PbSection
+  },
+  props: {
+    ...commonProps,
+    slice: {
+      type: Object,
+      required: true,
+      default () {
+        return {}
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .content-cards {
+    display: flex;
+    padding: 0 50px;
+
+    &__item {
+      padding: 0 30px;
+    }
+
+    &__image {
+      height: 300px;
+    }
+  }
+</style>
--- slices/index.js
+++ slices/index.js
@@ -1,2 +1,3 @@
 export { default as MainBanner } from './MainBanner'
 export { default as TestimonialCarousel } from './TestimonialCarousel'
+export { default as ContentCards } from './ContentCards'
