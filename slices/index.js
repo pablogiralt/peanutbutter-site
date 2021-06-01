0c75f62b20048acca0b1a80520e7d7c7daf044da
Pablo Giralt <pablogiralt@gmail.com>
2021-06-01 17:57:16 +0200

    Contact form slice skeleton

--- slices/index.js
+++ slices/index.js
@@ -1,3 +1,4 @@
 export { default as MainBanner } from './MainBanner'
 export { default as TestimonialCarousel } from './TestimonialCarousel'
 export { default as ContentCards } from './ContentCards'
+export { default as ContactForm } from './ContactForm'
