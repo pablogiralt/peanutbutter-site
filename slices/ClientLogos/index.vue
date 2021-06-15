ba83c587c117375ca660cd68bda88f17641bb16a
Pablo Giralt <pablogiralt@gmail.com>
2021-06-15 13:30:21 +0200

    #17 Client Logos component skeleton

---
+++ slices/ClientLogos/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <section class="logos">
+    <prismic-rich-text :field="slice.primary.title" class="logos__title" />
+    <prismic-rich-text :field="slice.primary.description" class="logos__description" />
+
+    <ul class="logos__list">
+      <li v-for="(item, index) in slice.items" :key="index" class="logos__list__item">
+        <img :src="item.logo.url" :alt="item.logo.url" class="logos__list__item__image">
+      </li>
+    </ul>
+  </section>
+</template>
+
+<script>
+export default {
+  props: {
+    slice: {
+      type: Object,
+      required: true,
+      default () {
+        return {}
+      }
+    }
+  },
+
+  mounted () {
+    console.log(this.slice)
+  }
+}
+</script>
+
+<style scoped>
+</style>
