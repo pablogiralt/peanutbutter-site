15297f5af4d1da5c797e3457457b125286248be0
Pablo Giralt <pablogiralt@gmail.com>
2021-06-15 22:36:05 +0200

    #20 fetch social media links from prismic

--- store/index.js
+++ store/index.js
@@ -4,13 +4,20 @@ export const state = () => ({
   prismicLocales: {
     en: 'en-us',
     es: 'es-es'
-  }
+  },
+  settings: {}
 })
 
 export const mutations = {
   SET_LANG (state, locale) {
     if (state.locales.includes(locale)) {
       state.locale = locale
+    }
+  },
+
+  SET_SETTINGS (state, settings) {
+    if (settings && settings.data) {
+      state.settings = settings.data
     }
   }
 }
