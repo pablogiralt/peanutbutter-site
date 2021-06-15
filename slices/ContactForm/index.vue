97453db0f9376f8ca006bdf1f3e7cc06ddc97776
Pablo Giralt <pablogiralt@gmail.com>
2021-06-15 17:47:25 +0200

    #16 Create ContactForm slice (which calls ContacForm component)

---
+++ slices/ContactForm/index.vue
@@ -0,0 +1,44 @@
+<template>
+  <ContactForm :title="slice.primary.title" :description="slice.primary.description" />
+</template>
+
+<script>
+import ContactForm from '../../components/ContactForm'
+
+export default {
+
+  components: {
+    ContactForm
+  },
+
+  props: {
+    slice: {
+      type: Object,
+      required: true,
+      default () {
+        return {}
+      }
+    }
+  },
+
+  created () {
+    console.log(this.slice.primary.title)
+  }
+}
+</script>
+
+<style scoped>
+.section {
+  position: relative;
+  background: #f7f7f7;
+  color: #111;
+  padding: 4em;
+  text-align: center;
+}
+a {
+  color: #111;
+}
+.title {
+  margin-bottom: 2em;
+}
+</style>
