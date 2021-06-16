c50f827e50a089a58d05c3e04cf35d1fa1b7bfa7
Pablo Giralt <pablogiralt@gmail.com>
2021-06-16 20:10:10 +0200

    #28 Fix homepage menu item always having active class

--- components/Navigation.vue
+++ components/Navigation.vue
@@ -14,7 +14,7 @@
     <nav :class="{ 'is-active' : menuOpen }" class="main-nav">
       <ul class="main-nav__list">
         <li v-for="(item, i) in mainMenu" :key="`menu-item-${i}`" class="main-nav__list__item" @click="toggleMenu">
-          <!-- 
+          <!--
             Thanks to exact-path, this link will only be active if the path is exact to that of url
             We may need to conditionally apply this when we have to level url like blog/post-titlee
             https://router.vuejs.org/api/#exact
