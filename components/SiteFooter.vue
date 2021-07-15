3176287074141a45252d2918d6f79c0c90e48f70
Javier <jk0reano@gmail.com>
2021-07-15 19:53:55 +0200

    add peanut in footer

--- components/SiteFooter.vue
+++ components/SiteFooter.vue
@@ -1,59 +1,90 @@
 <template>
-  <footer class="footer">
-    <div class="footer__container">
-      <div class="footer__col footer__col--info">
-        <a href="#" rel="noopener noreferrer" class="footer__mail-link">
-          hello [at] peanutbutter.es
-        </a>
+  <div class="footer-wrapper">
+    <footer class="footer">
+      <div class="footer__container">
 
-        <div class="footer__social">
-          <a
-            v-for="(item, index) in socialMedia"
-            :key="index"
-            :href="item.link.url"
-            class="footer__social__link"
-            :target="item.link.target && item.link.target=='_blank' ? '_blank' : '_self'"
-          >
-            <img :src="item.icon.url" :alt="item.icon.alt || item.name || ''">
+        <div class="footer__col footer__col--info">
+          <a href="#" rel="noopener noreferrer" class="footer__mail-link">
+            hello [at] peanutbutter.es
           </a>
-        </div>
-      </div>
 
-      <div class="footer__col footer__col--logo">
-        <nuxt-link :to="logoLink" class="footer__logo-link">
-          <img class="logo" src="~/static/logo-peanut-butter-collective.svg">
-        </nuxt-link>
-      </div>
-
-      <div class="footer__col footer__col--nav">
-        <div class="footer__nav">
-          <div class="footer__nav__title">
-            {{ $t('footer.legal') }}
+          <div class="footer__social">
+            <a
+              v-for="(item, index) in socialMedia"
+              :key="index"
+              :href="item.link.url"
+              class="footer__social__link"
+              :target="item.link.target && item.link.target=='_blank' ? '_blank' : '_self'"
+            >
+              <img :src="item.icon.url" :alt="item.icon.alt || item.name || ''">
+            </a>
           </div>
+        </div>
 
-          <nav class="footer__legal-nav">
-            <ul class="footer__legal-nav__list">
-              <li v-for="(item, i) in footerMenu" :key="`menu-item-${i}`" class="footer__legal-nav__list__item">
-                <prismic-link :field="item.link" class="footer__legal-nav__list__item__link">
-                  {{ item.label }}
-                </prismic-link>
-              </li>
-            </ul>
-          </nav>
+        <div class="footer__col footer__col--logo">
+          <nuxt-link :to="logoLink" class="footer__logo-link">
+            <img class="logo" src="~/static/logo-peanut-butter-collective.svg">
+          </nuxt-link>
+        </div>
+
+        <div class="footer__col footer__col--nav">
+          <div class="footer__nav">
+            <div class="footer__nav__title">
+              {{ $t('footer.legal') }}
+            </div>
+
+            <nav class="footer__legal-nav">
+              <ul class="footer__legal-nav__list">
+                <li v-for="(item, i) in footerMenu" :key="`menu-item-${i}`" class="footer__legal-nav__list__item">
+                  <prismic-link :field="item.link" class="footer__legal-nav__list__item__link">
+                    {{ item.label }}
+                  </prismic-link>
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </div>
+
+        <div class="footer__col footer__col--copy">
+          <div class="footer__copy">
+            Hypogaea SL &copy; {{ currentDate.getFullYear() }}
+          </div>
         </div>
       </div>
+    </footer>
 
-      <div class="footer__col footer__col--copy">
-        <div class="footer__copy">
-          Hypogaea SL &copy; {{ currentDate.getFullYear() }}
-        </div>
-      </div>
+    <div class="peanut">
+      <img class="peanut__hands" src="~/static/peanut_hands.png">
+      <img v-infocus="'in-view'" class="peanut__head" src="~/static/peanut_head.png">
     </div>
-  </footer>
+  </div>
 </template>
 
 <script>
 export default {
+  directives: {
+    infocus: {
+      isLiteral: true,
+      inserted: (el, binding, vnode) => {
+        const f = () => {
+          const rect = el.getBoundingClientRect()
+          const inView = (
+            rect.width > 0 &&
+            rect.height > 0 &&
+            rect.top >= 0 &&
+            rect.bottom <= (window.innerHeight || document.documentElement.clientHeight)
+          )
+          if (inView) {
+            el.classList.add(binding.value)
+            window.removeEventListener('scroll', f)
+          }
+        }
+        window.addEventListener('scroll', f)
+        f()
+      }
+    }
+  },
+
   props: {
     footerMenu: {
       type: Array,
@@ -83,7 +114,13 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.footer-wrapper {
+  position: relative;
+}
+
 .footer {
+  position: relative;
+  z-index: 2;
   background-color: $black;
   padding: rem(80px) rem(16px) rem(32px);
 
@@ -225,6 +262,57 @@ export default {
     color: $grey-60;
     font-size: rem(12px);
     line-height: 1.33;
+  }
+}
+
+.peanut {
+  display: none;
+
+  @media (min-width: $md) {
+    position: absolute;
+    right: 10%;
+    top: -12px;
+    display: block;
+
+    &__hands {
+      width: 200px;
+      position: relative;
+      z-index: 2;
+    }
+
+    &__head {
+      position: absolute;
+      left: 50%;
+      transform: translate3d(-50%, 50px, 0);
+      z-index: 1;
+      width: 100px;
+
+      &.in-view {
+        animation: popIn 10s cubic-bezier(0.250, 0.460, 0.450, 0.940) both;
+      }
+    }
+
+    @keyframes popIn {
+      0% {
+        transform: translate3d(-50%, 50px, 0);
+      }
+
+      10% {
+        transform: translate3d(-50%, -20px, 0);
+      }
+
+      40% {
+        transform: translate3d(-50%, -40px, 0);
+      }
+
+      60% {
+        transform: translate3d(-50%, 50px, 0);
+      }
+
+      100% {
+        transform: translate3d(-50%, -60px, 0);
+      }
+    }
   }
 }
 </style>
